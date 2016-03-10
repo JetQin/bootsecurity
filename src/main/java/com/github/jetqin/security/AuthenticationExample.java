1b2f5cc80d7e9a97b0653cabf235bedb32a034e2
JetQin <qinjet@gmail.com>
2016-03-11 00:02:49 +0800

    first project creation

---
+++ src/main/java/com/github/jetqin/security/AuthenticationExample.java
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////////////////////
+//// 
+//// Copyright (C) 2001-2016 the original author or authors.
+////
+//// Licensed to the Apache Software Foundation (ASF) under one or more
+//// contributor license agreements.  See the NOTICE file distributed with
+//// this work for additional information regarding copyright ownership.
+//// The ASF licenses this file to You under the Apache License, Version 2.0
+//// (the "License"); you may not use this file except in compliance with
+//// the License.  You may obtain a copy of the License at
+////
+////      http://www.apache.org/licenses/LICENSE-2.0
+////
+//// Unless required by applicable law or agreed to in writing, software
+//// distributed under the License is distributed on an "AS IS" BASIS,
+//// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//// See the License for the specific language governing permissions and
+//// limitations under the License.
+////
+//// date   : 2016年3月8日
+//// 
+//////////////////////////////////////////////////////////////////////////////////
+//package com.github.jetqin.security;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+//import org.springframework.security.access.AccessDecisionManager;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//
+///**
+// * @author jet
+// *
+// */
+//public class AuthenticationExample
+//{
+//  private static AuthenticationManager am = new SampleAuthenticationManager();
+//  
+//  private static AccessDecisionManager adm = new SampleAccessDecisionManager();
+//
+//  public static void main ( String[] args ) throws Exception
+//  {
+//    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//
+//    while (true)
+//    {
+//      System.out.println("Please enter your username:");
+//      String name = in.readLine();
+//      System.out.println("Please enter your password:");
+//      String password = in.readLine();
+//      try
+//      {
+//        Authentication request = new UsernamePasswordAuthenticationToken(name, password);
+//        Authentication result = am.authenticate(request);
+//        SecurityContextHolder.getContext().setAuthentication(result);
+//        break;
+//      }
+//      catch (AuthenticationException e)
+//      {
+//        System.out.println("Authentication failed: " + e.getMessage());
+//      }
+//    }
+//    System.out.println("Successfully authenticated. Security context contains: "
+//        + SecurityContextHolder.getContext().getAuthentication());
+//  }
+//
+//}
---
+++ src/main/java/com/github/jetqin/security/SampleAccessDecisionManager.java
@@ -0,0 +1,68 @@
+//////////////////////////////////////////////////////////////////////////////////
+//// 
+//// Copyright (C) 2001-2016 the original author or authors.
+////
+//// Licensed to the Apache Software Foundation (ASF) under one or more
+//// contributor license agreements.  See the NOTICE file distributed with
+//// this work for additional information regarding copyright ownership.
+//// The ASF licenses this file to You under the Apache License, Version 2.0
+//// (the "License"); you may not use this file except in compliance with
+//// the License.  You may obtain a copy of the License at
+////
+////      http://www.apache.org/licenses/LICENSE-2.0
+////
+//// Unless required by applicable law or agreed to in writing, software
+//// distributed under the License is distributed on an "AS IS" BASIS,
+//// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//// See the License for the specific language governing permissions and
+//// limitations under the License.
+////
+//// date   : 2016年3月8日
+//// 
+//////////////////////////////////////////////////////////////////////////////////
+//package com.github.jetqin.security;
+//
+//import java.util.Collection;
+//
+//import org.springframework.security.access.AccessDecisionManager;
+//import org.springframework.security.access.AccessDeniedException;
+//import org.springframework.security.access.ConfigAttribute;
+//import org.springframework.security.authentication.InsufficientAuthenticationException;
+//import org.springframework.security.core.Authentication;
+//
+///**
+// * @author jet
+// *
+// */
+//public class SampleAccessDecisionManager implements AccessDecisionManager
+//{
+//
+//  /* (non-Javadoc)
+//   * @see org.springframework.security.access.AccessDecisionManager#decide(org.springframework.security.core.Authentication, java.lang.Object, java.util.Collection)
+//   */
+//  public void decide ( Authentication arg0, Object arg1, Collection<ConfigAttribute> arg2 )
+//      throws AccessDeniedException, InsufficientAuthenticationException
+//  {
+//    // TODO Auto-generated method stub
+//    
+//  }
+//
+//  /* (non-Javadoc)
+//   * @see org.springframework.security.access.AccessDecisionManager#supports(org.springframework.security.access.ConfigAttribute)
+//   */
+//  public boolean supports ( ConfigAttribute arg0 )
+//  {
+//    // TODO Auto-generated method stub
+//    return false;
+//  }
+//
+//  /* (non-Javadoc)
+//   * @see org.springframework.security.access.AccessDecisionManager#supports(java.lang.Class)
+//   */
+//  public boolean supports ( Class<?> arg0 )
+//  {
+//    // TODO Auto-generated method stub
+//    return false;
+//  }
+//
+//}
