c2afefd0525efc7bbc40b3a1d8b0d9343231d191
JetQin <qinjet@gmail.com>
2016-03-16 08:06:11 +0800

    fix bugs

--- src/main/java/com/github/jetqin/startup/StartupApplication.java
+++ src/main/java/com/github/jetqin/startup/StartupApplication.java
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -39,7 +40,7 @@ import com.github.jetqin.repository.CustomerRepository;
  */
 
 @EnableJpaRepositories
-@EnableAutoConfiguration
+@SpringBootApplication
 //@ComponentScan(basePackages={"com.github.jetqin"})
 //@EnableOrmConfiguration(showSql=false,packageToScan="com.github.jetqin.domain",enableCache=true)
 public class StartupApplication
@@ -54,24 +55,24 @@ public class StartupApplication
     SpringApplication.run(StartupApplication.class);
   }
   
-  @Bean
-  public CommandLineRunner demo( CustomerRepository repository) {
-      return (args) -> {
-          // save a couple of customers
-          repository.save(new Customer("Jack", "Bauer"));
-          repository.save(new Customer("Chloe", "O'Brian"));
-          repository.save(new Customer("Kim", "Bauer"));
-          repository.save(new Customer("David", "Palmer"));
-          repository.save(new Customer("Michelle", "Dessler"));
-
-          // fetch all customers
-          log.info("Customers found with findAll():");
-          log.info("-------------------------------");
-          for (Customer customer : repository.findAll()) {
-              log.info(customer.toString());
-          }
-          log.info("");
-
-      };
-  }
+//  @Bean
+//  public CommandLineRunner demo( CustomerRepository repository) {
+//      return (args) -> {
+//          // save a couple of customers
+//          repository.save(new Customer("Jack", "Bauer"));
+//          repository.save(new Customer("Chloe", "O'Brian"));
+//          repository.save(new Customer("Kim", "Bauer"));
+//          repository.save(new Customer("David", "Palmer"));
+//          repository.save(new Customer("Michelle", "Dessler"));
+//
+//          // fetch all customers
+//          log.info("Customers found with findAll():");
+//          log.info("-------------------------------");
+//          for (Customer customer : repository.findAll()) {
+//              log.info(customer.toString());
+//          }
+//          log.info("");
+//
+//      };
+//  }
 }
